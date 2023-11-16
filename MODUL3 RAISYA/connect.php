038e483b6ac8e7e5ea99ef1e03e218aa6f1e44f1
Raisya Salsabila <147051450+salsabilaraisya@users.noreply.github.com>
2023-11-16 11:44:44 +0700

    Add files via upload

---
+++ MODUL3 RAISYA/connect.php
@@ -0,0 +1,12 @@
+<!-- File ini berisi koneksi dengan database yang telah di import ke phpMyAdmin kalian -->
+
+
+<?php
+// Buatlah variable untuk connect ke database yang telah di import ke phpMyAdmin
+
+// 
+  
+// Buatlah perkondisian jika tidak bisa terkoneksi ke database maka akan mengeluarkan errornya
+
+// 
+?>
---
+++ MODUL3 RAISYA/create.php
@@ -0,0 +1,33 @@
+<!-- Pada file ini kalian membuat coding untuk logika create / menambahkan mobil pada showroom -->
+
+<?php
+// (1) Jangan lupa sertakan koneksi database dari yang sudah kalian buat yaa
+
+
+// 
+
+// (2) Buatlah perkondisian untuk memeriksa apakah permintaan saat ini menggunakan metode POST
+
+// 
+
+// (3) Jika sudah coba deh kalian ambil data dari form (CLUE : pakai POST)
+
+    // a. Ambil data nama mobil
+
+    // b. Ambil data brand mobil
+
+    // c. Ambil data warna mobil
+
+    // d. Ambil data tipe mobil
+
+    // e. Ambil data harga mobil
+
+    // (4) Kalau sudah, kita lanjut Query / Menambahkan data pada SQL (Disini ada perintah untuk SQL), Masukkan ke tabel showroom_mobil (include setiap nama column)
+
+
+    // (5) Buatkan kondisi jika eksekusi query berhasil
+
+    // (6) Jika terdapat kesalahan, buatkan eksekusi query gagalnya 
+
+// (7) Tutup koneksi ke database setelah selesai menggunakan database
+?>
